40dae1308fe5929702ed833fa61b5127cd0f43dd
simonradev <simonradev@gmail.com>
2017-03-13 20:13:24 +0200

    Now the snake is getting bigger when it eats food.

--- Position/SnakeUpdate.cs
+++ Position/SnakeUpdate.cs
@@ -9,22 +9,19 @@
         public static bool UpdateTheSnake(Queue<Position> snake, Position newHeadPosition)
         {
             Position head = snake.Last();
-            snake.Dequeue();
             Position newHead = new Position(head.Row + newHeadPosition.Row, head.Col + newHeadPosition.Col);
 
             PositionCheck.CheckIfSnakeCollidesIntoItself(snake, newHead);
             bool snakeAteFood = PositionCheck.CheckIfTheSnakeHadSomeFood(newHead);
 
+            if (!snakeAteFood)
+            {
+                snake.Dequeue();
+            }
+
             snake.Enqueue(newHead);
 
             return snakeAteFood;
         } 
-
-        private static void MakeTheSnakeBiggerAfterItAteFood(Queue<Position> snake)
-        {
-            Position[] snakeHolder = snake.ToArray();
-
-
-        }
     }
 }
